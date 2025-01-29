3e535cd6ca39a48303790fabd1a4b1df56aa6544
Maria dutra <dutracosta1904@gmail.com>
2025-01-29 20:41:14 -0300

    postando o portfolio

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeInElements = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          // Adiciona um atraso de tempo para cada elemento
+          setTimeout(() => {
+            entry.target.classList.add("active");
+          }, index * 400); // Atraso de 300ms para cada elemento
+          observer.unobserve(entry.target); // Para observar apenas uma vez
+        }
+      });
+    },
+    { threshold: 0.2 } // Ativa quando 30% do elemento está visível
+  );
+
+  fadeInElements.forEach((element) => {
+    observer.observe(element); // Observa cada elemento com a classe .fade-in
+  });
+});
